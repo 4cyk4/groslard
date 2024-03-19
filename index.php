36b6ad0f1dacc6f1cea15e467054fd69e695c8b0
4cyk4 <124710673+4cyk4@users.noreply.github.com>
2024-03-19 10:37:28 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+    include "include/haut.inc";
+?>
+<main>
+    <h2>Une présentation sympa</h2>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam iusto sint, nulla, sequi nisi illum animi dolores cumque rerum harum placeat fugit ut voluptatibus nemo nihil vero architecto praesentium eaque. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus eius iste quia repudiandae. Voluptate, ex ut corporis assumenda nesciunt, reiciendis soluta eligendi aspernatur, cumque itaque eius non temporibus impedit numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias esse minima eos cum consequatur delectus reprehenderit vero. Voluptas, quibusdam facilis veritatis error doloremque ex odit numquam quisquam sunt iusto dolores! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam quod repellat nulla hic suscipit esse. Magni, porro doloremque? Assumenda, commodi eius omnis officia neque animi reiciendis quasi. Dicta, deleniti harum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, voluptas iste eaque totam nisi maxime! At cupiditate consectetur est. Sunt dicta accusantium necessitatibus unde cum. Autem ipsum aliquid optio quae.</p>
+    <img src="<?= ROOT ?>images/image5.jpg" alt="image sympa">
+    <img src="<?= ROOT ?>images/image5.jpg" alt="image sympa">
+    <img src="<?= ROOT ?>images/image5.jpg" alt="image sympa">
+</main>
+<?php 
+    include "include/bas.inc";
+?>
